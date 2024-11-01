349b97416e6af038eb3d1adeaa72cf50edbc42e2
Aayush Shukla <oreus.aks@gmail.com>
2024-11-01 18:49:01 +0530

    feat: added validation to task_name field

--- models.py
+++ models.py
@@ -1,12 +1,18 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from typing import Literal
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey, UniqueConstraint
 from database import Base
 
 
-class Task(BaseModel):
+class TaskCreate(BaseModel):
     task_name: str
     status: Literal["active", "completed"]
+
+    @field_validator('task_name')
+    def task_name_must_not_be_empty(self, value):
+        if not value.strip():
+            raise ValueError("Task name cannot be empty")
+        return value
 
 class Users(Base):
     __tablename__ = "users"
@@ -17,7 +23,7 @@ class Users(Base):
 
 class Tasks(Base):
     __tablename__ = "tasks"
-
+    __table_args__ = (UniqueConstraint('task_name', name='uq_task_name'),)
 
     id = Column(Integer, primary_key=True, index=True)
     task_name = Column(String, unique=True)
