22fbe0c0a6aa44e019b181a2ae8f5825971e27fe
Aayush Shukla <oreus.aks@gmail.com>
2024-11-01 18:07:59 +0530

    feat: revamping the database, user only gets to know his own data

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from pydantic import BaseModel
-from typing import List, Literal
-from sqlalchemy import Column, String, Integer
+from typing import Literal
+from sqlalchemy import Column, String, Integer, ForeignKey
 from database import Base
 
 
 class Task(BaseModel):
     task_name: str
-    status: Literal["all", "active", "completed"]
+    status: Literal["active", "completed"]
+    associated_user_id: int
 
 class Users(Base):
     __tablename__ = "users"
@@ -14,6 +15,15 @@ class Users(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True)
     hashed_password = Column(String)
+
+class Tasks(Base):
+    __tablename__ = "tasks"
+
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True)
+    status = Column(String)
+    associated_user_id = Column(Integer, ForeignKey("users.id"))
 
 
 class ToDoList(Base):
